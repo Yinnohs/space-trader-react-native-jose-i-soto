67fefbcd5d864a7c4b4f6863908b08a99234c397
josesoto <josesoto@itop.es>
2023-02-19 17:47:01 +0000

    feat(screen): ended home screen functionality

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,7 +1,7 @@
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useContext, useEffect, useState } from 'react';
-import {View,Text, Image} from 'react-native'
+import {View,Text, Image, Pressable} from 'react-native'
 import { getUserinformation } from '../../api';
 import { AppContext, UserContext } from '../../context';
 import { StackRoutes } from '../../types/screens.types';
@@ -25,18 +25,37 @@ export const HomeScreen = (props: Props) => {
 
   useEffect(()=>{
     handleInit()
-  }, [])
+  }, [data])
   
   return (
-    <View className='flex-1 items-center justify-center bg-p-5' >
-      <View className='flex-1'>
-        <Image source={require('../../../assets/imgs/user.png')}></Image>
+    <View className='flex-1 items-center justify-center bg-p-4 gap-4' >
+      <View className=' flex-col w-[80vw] h-[20vh] justify-center items-center bg-p-2 shadow-lg rounded-lg overflow-hidden gap-2'>
+        <Image className='rounded-full w-20 h-20' 
+        source={require('../../../assets/imgs/user.png')}/>
+        <Text className='text-secondary text-2xl'>
+          {userData.user.username}
+        </Text>
       </View>
-      <View className='flex-1'>
 
-      </View>
-      <View className='flex-1'>
+      <Pressable
+      onPress={()=> navigation.navigate('Loans')} 
+      className='flex-row w-[80vw] h-[20vh] justify-center items-center bg-p-2 shadow-lg rounded-lg'>
+        <Text className='text-secondary text-2xl'>
+          {userData.user.credits} crd
+        </Text>
+      </Pressable>
 
+      <View className='flex-col w-[80vw] h-[30vh] justify-center  items-center bg-p-1 shadow-lg rounded-lg'>
+        <View className='flex-row w-full h-[10vh] justify-center items-center ' >
+          <Image className='rounded-full w-14 h-14' 
+           source={require('../../../assets/imgs/space-ship.png')}/>
+          <Text className='text-secondary text-2xl'>{userData.user.shipCount}</Text>
+        </View>
+        <View className='flex-row w-full h-[10vh] justify-center items-center'>
+          <Image className='rounded-full w-14 h-14' 
+          source={require('../../../assets/imgs/cartoon.png')}/>
+          <Text className='text-secondary text-2xl'>{userData.user.structureCount}</Text>
+        </View>
       </View>
     </View>
   )
